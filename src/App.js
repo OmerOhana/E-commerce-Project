6a5d18011c3e6d9925383b7fa028a7ad4f8a3edb
OmerOhana <oohana13@gmail.com>
2020-07-16 14:54:25 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage';
-import ShopPage from './pages/shop/shop.jsx';
+import ShopPage from './pages/shop/shop';
 import Header from './Components/header/header';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path = '/' component={HomePage} />
         <Route path = '/shop' component={ShopPage} />
+        <Route path = '/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
