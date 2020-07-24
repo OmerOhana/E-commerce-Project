e0261f9a5af94c9b1cb81ec9d73a4678b5b5fdb3
OmerOhana <oohana13@gmail.com>
2020-07-24 14:19:37 +0300

    Updates

---
+++ precache-manifest.4e8322f1d751eaaee8e44f503c132107.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d8105801c522f06e1f3433c60a8c0c",
+    "url": "/E-commerce-Project/index.html"
+  },
+  {
+    "revision": "0fc02a8f963b4f41eb1a",
+    "url": "/E-commerce-Project/static/css/main.477029b0.chunk.css"
+  },
+  {
+    "revision": "9b9aec1545e0f4b3398f",
+    "url": "/E-commerce-Project/static/js/2.c5b16980.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/E-commerce-Project/static/js/2.c5b16980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc02a8f963b4f41eb1a",
+    "url": "/E-commerce-Project/static/js/main.c2d8e64a.chunk.js"
+  },
+  {
+    "revision": "63b04211e16ea3b1a41b",
+    "url": "/E-commerce-Project/static/js/runtime-main.5ddf90b3.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/E-commerce-Project/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/E-commerce-Project/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
